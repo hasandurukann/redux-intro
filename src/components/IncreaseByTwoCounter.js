f6279b484b384755ff73f5589da0339c95c38ab1
Hasan Durukan <15290668@ogrenci.ankara.edu.tr>
2020-08-03 19:10:54 +0300

    son

---
+++ src/components/IncreaseByTwoCounter.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { increaseByTwoCounter } from "../redux/actions/counterActions";
+import { bindActionCreators } from "redux";
+
+class IncreaseByTwoCounter extends Component {
+  render() {
+    return (
+      <div>
+        <button
+          onClick={(e) => {
+            this.props.dispatch(increaseByTwoCounter());
+          }}
+        >
+          +2
+        </button>
+      </div>
+    );
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return { actions: bindActionCreators(increaseByTwoCounter, dispatch) };
+}
+
+export default connect(mapDispatchToProps)(IncreaseByTwoCounter);
---
+++ src/components/IncreaseCounter.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { increaseCounter } from "../redux/actions/counterActions";
+import { bindActionCreators } from "redux";
+
+class IncreaseCounter extends Component {
+  render() {
+    return (
+      <div>
+        <button
+          onClick={(e) => {
+            this.props.dispatch(increaseCounter());
+          }}
+        >
+          +1
+        </button>
+      </div>
+    );
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return { actions: bindActionCreators(increaseCounter, dispatch) };
+}
+
+export default connect(mapDispatchToProps)(IncreaseCounter);
